1efe59715ba42278bb6ddbd0c5bedd9e373b75f9
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2021-07-26 12:29:04 +0300

    Commit 5 HW9

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -5,7 +5,6 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -41,13 +40,8 @@ public class Attach {
         );
     }
 
-    //    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-//    public static String addVideo(String sessionId) {
-//        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-//                + getVideoUrl(sessionId)
-//                + "' type='video/mp4'></video></body></html>";
-//    }
-//
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+
     public static void addVideo(String sessionId) {
         URL videoUrl = getVideoUrl(sessionId);
         if (videoUrl != null) {
