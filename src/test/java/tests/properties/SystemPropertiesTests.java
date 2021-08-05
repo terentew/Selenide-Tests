9283b2855fa14b159d23c2564ae363ae58579b3c
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2021-08-05 18:05:28 +0300

    Commit 1 HW10

---
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,14 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("properties")
+public class SystemPropertiesTests {
+
+    @Test
+    void test1(){
+
+
+    }
+}
